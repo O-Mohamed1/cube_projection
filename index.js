1c523a0d8a3e89eea42491a4925726e07a62e058
Mohamed <mohamedps169@gmail.com>
2024-03-01 14:27:59 -0500

    Improvements Still bored so I'm adding more stuff. Added outline to the cube Added opacity to make the dots get lighter as they move further away from the user Currently working on 2 things: 1 is a firework, where the dots get darker as they are further from the center, and 2 is a way to control the viewing angle of the cube by making the distance between the first and last slice an independent variable

--- index.js
+++ index.js
@@ -23,13 +23,56 @@ function submitted(){
 
 
 
-function printSquare(squarePositionX, linePositionY, sliceIndex) {//prints each square
+function printSquare(squarePositionX, linePositionY, indices) {//prints each square
     const SQUARE = document.createElement("div");
     SQUARE.classList.add("square");
 
     SQUARE.style.marginLeft = `${squarePositionX}px`;//distance between squares
     SQUARE.style.marginTop = `${linePositionY}px`; //distance between lines
-    SQUARE.style.zIndex = `${sliceIndex + 2}px`; //move upwards in Z direction to avoid collision, +2 because sliceIndex starts at 0 and background z-index is 1
+    SQUARE.style.zIndex = `${indices[0] + 2}px`; //move upwards in Z direction to avoid collision, +2 because sliceIndex starts at 0 and background z-index is 1
+    
+    let further = indices[0]/width;
+    SQUARE.style.backgroundColor = `rgba(243, 146, 202,${1-further})`; //make the squares darker as they get further away
+    SQUARE.style.width = `${8}px`; //!make the squares smaller as they get further away
+    SQUARE.style.height = `${8}px`; //!^
+    //could make it a firework by making the squares more opaque as they near the center of the slices and lines and squares, so:
+    /*
+    
+    let opacity=1
+    const halfwayPoint=width/2
+    let solved = 0
+    function firework(indices){
+        solved++
+        if (solved>3){
+            return opacity
+        }
+        for(let i = 0; i<indices.length; i++){
+            if(indices[0]<halfwayPoint){
+                opacity*=indices[i]/halfwayPoint
+            }else if(indices[0]>halfwayPoint){
+                opacity*=(width-indices[i])/halfwayPoint
+            }else {
+                opacity*=1
+            }
+        }
+        firework(indices[i])
+        return opacity 
+    }
+    
+    
+
+    */
+
+    let squareFirstOrLast = (indices[2]==0 || indices[2]==width)
+    let lineFirstOrLast = (indices[1] == 0 || indices[1] == width)
+    let sliceFirstOrLast = (indices[0]==0 || indices[0]==width)
+    
+    if(lineFirstOrLast && squareFirstOrLast){ //make other edges red
+        SQUARE.style.backgroundColor = `rgba(255, 0, 0,${1-((indices[0]-3)/width)})`;
+    }
+    if(sliceFirstOrLast && (lineFirstOrLast || squareFirstOrLast)){ //make first and last face's edges red
+        SQUARE.style.backgroundColor = `rgba(255, 0, 0,${1-((indices[0]-3)/width)})`;
+    }
 
     WINDOW.appendChild(SQUARE);
     
@@ -38,6 +81,13 @@ function printSquare(squarePositionX, linePositionY, sliceIndex) {//prints each 
 
 //calculate a cube and save each dot in a matrix
 function calculateCube(width) {//could just move everything from other loops to here. That way I act on the data as it is produced
+    class Square{
+        constructor(opacity, size, color){
+            this.opacity = opacity;
+            this.size = size;
+            this.color = color;
+        }
+    }
     if (width < 2) {
         console.log("wyd");
     }
@@ -46,7 +96,7 @@ function calculateCube(width) {//could just move everything from other loops to 
         for (let j = 0; j < width+1; j++) {
             let line = [];
             for (let k = 0; k < width+1; k++) {
-                line.push(1); // add 1 square to a line, width number of times
+                line.push(new Square(1,8,"red")); // add 1 square to a line, width number of times
             }
             slice.push(line); // add 1 line of width squares to a slice, width number of times
         }
@@ -54,37 +104,45 @@ function calculateCube(width) {//could just move everything from other loops to 
     }
     return cube;
 }
+//endpoint = last slice should start at (totalDistance/4) then the square distance changes based on endpoint
+//(totalDistance * (.9325 ** sliceIndex) / width)
+//endpoint should change this^ part of the equation
 
 //calculate distance between squares based on how deep in the cube the square is
 function calculateSquareDistance(sliceIndex) { // x**0 == 1 so first case still works
-    return (totalDistance * (.9375 ** sliceIndex) / width); //exponentially shorten distances between squares in subsequent slices
+    return (totalDistance * (.9325 ** sliceIndex) / width); //exponentially shorten distances between squares in subsequent slices
 }
 
 //func4 will forEach the arrays in the matrix and print them as square elements on html
 function outputCube() {
+    
     calculateCube(width);
     let squareDistance = calculateSquareDistance(0); //starting square distance
 
     let sliceLocation = 0;   //keeps track of where current slice will be printed, first slice/line/square starts at the top left
     let linePositionY = 0;   //keeps track of where current line will be printed
     let squarePositionX = 0; //keeps track of where current square will be printed, first square at the start of the line
-
-    function lineDisplacement(sliceIndex = -1) { //this is the gap added to each subsequent line
+    let indices = [0,0,0];   //keeps track of indices of slices, lines, and squares in that order
+    
+    function lineDisplacement() { //this is the gap added to each subsequent line
         return ((totalDistance - 15 - squareDistance * width) / 2);
     };
 
     //unique about each slice: they each start at a different position, and have different square distances
     cube.forEach((slice, sliceIndex) => { //go forward slice by slice in the cube, starting with the front face
-        squareDistance = calculateSquareDistance(sliceIndex); //goes at start of each slice, each one has unique distance
+        indices[0]=sliceIndex; //save sliceIndex
+        squareDistance = calculateSquareDistance(indices[0]); //goes at start of each slice, each one has unique distance
         sliceLocation = lineDisplacement(); //this is how much to move each slice, changes per slice
-
+        
         linePositionY = sliceLocation - 0;
         //each line is sliceLocation down, and right
-        slice.forEach(line => { //go down line by line in the slice, starting with the top line
+        slice.forEach((line, lineIndex)=> { //go down line by line in the slice, starting with the top line
+            indices[1]=lineIndex; //save lineIndex
             squarePositionX = sliceLocation - 0; //position of first square of each line
-
-            line.forEach(square => { //go right square by square in the line, starting with the left
-                printSquare(squarePositionX, linePositionY, sliceIndex); //print square
+            
+            line.forEach((square, squareIndex) => { //go right square by square in the line, starting with the left
+                indices[2]=squareIndex //save squareIndex
+                printSquare(squarePositionX, linePositionY, indices); //print square
                 squarePositionX += squareDistance -0; //next square's position relative to the left side of the image, goes after each square is printed
 
             })
