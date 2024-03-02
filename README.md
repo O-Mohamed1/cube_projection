1b2241a8037af95086a6862c87d7d94611778cd3
Mohamed <mohamedps169@gmail.com>
2024-03-02 13:30:41 -0500

    Refactored code, improved time and space complexity It is around 5x faster to render HI than it was originally Could be further improved by adding a Z coordinate based on square.Zindex to change the viewing angle based on camera's position, but at that point its too much I think. I would rather use actual graphics APIs and learn to make a C++ or C# program.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ Projects a 3D cube of dots onto a 2D surface.
 (Very rushed, didn't implement recursion or other simpler functions)
 
 It can render 3D objects inside the cube, but it cannot move the camera around
+Starting width is 80, which might take too long to load on some devices
--- index.js
+++ index.js
@@ -4,10 +4,14 @@ const WINDOW = document.getElementById("image"); //where cube will be output
 //and I could make them shrink or go invisible as well, which a regular display can't do. Moving the camera will be tough though,
 //that involves a lot of math
 
-let totalDistance = 800; //distance of entire cube
-let width = 39; //width of cube in points, but the actual width of the cube will be width+1 so if width==9, the cube will be 10^3
+//the bad thing about this program is that it doesn't actually take into account the Z coordinate, so I can't move things back and forth
+//by an X number of pixels, all it uses is the Z index to calculate the X and Y coordinates. This is because the camera is static
 
-function submitted() {
+let totalDistance = 800; //distance of entire cube
+let width = 79; //width of cube in points, but the actual width of the cube will be width+1 so if width==9, the cube will be 10^3
+//I've been doubling the default value as time and space are more efficiently used, started with 10^3 cube, then 20, 40, and now 80
+
+function submitted() { //HTML form submit
     let input1 = document.getElementById("width"); //save width input
     let newWidth = Number(input1.value) - 1;
     width = newWidth;
@@ -15,62 +19,84 @@ function submitted() {
     let newDistance = Number(input2.value);
     totalDistance = newDistance;
     WINDOW.innerHTML = "";
+    WINDOW.style.width = `${newDistance}px`;
+    WINDOW.style.height = `${newDistance}px`;
     outputCube();
     return;
 }
 
-function render3D(square){
-    const [x,y,z]=[square.Xindex,square.Yindex,square.Zindex]
+function render3D(square){ //render one of 3 example objects, plus an optional red outline of the cube
+    const [x,y,z]=[square.Xindex,square.Yindex,square.Zindex] //to easily find the x,y,z index of each square. Note that the 
+    //actual unit of measurement is defined by calculateSquareDistance(square.Zindex), this will return a pixel value for how far apart
+    //the squares are depending on the Z level they're in, and the distance across the viewing window
+    square.show=0;
+
+    renderOutline = function(){//make a red outline around the cube:
+        let squareFirstOrLast = (x == 0 || x == width);
+        let lineFirstOrLast = (y == 0 || y == width);
+        let sliceFirstOrLast = (z == 0 || z == width);
+        if (lineFirstOrLast && squareFirstOrLast) { //make other edges red
+            square.color = `rgba(255, 0, 0,${1 - ((square.Zindex - 3) / width)})`;
+            square.show = 1
+        }
+        if (sliceFirstOrLast && (lineFirstOrLast || squareFirstOrLast)) { //make first and last face's edges red
+            square.color = `rgba(255, 0, 0,${1 - ((square.Zindex - 3) / width)})`;
+            square.show = 1
+        }
+    }
+    //renderOutline()
+
     renderPrism = function(){ //prints a green prism at the bottom of the cube. Proof of concept for 3D rendering
         if(!(y==0||y==width)){ 
             if (x>width/5&&x<width*3/5){
                 if(y>width*4/5){
                     if(z<width*3/5){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
+                        square.show = 1
                     }
                 }
             }
         }
     }
-
     //renderPrism()
-    function renderHi(){ //prints "HI"
-        if(!(y==0||y==width)){//avoid first and last lines, so they dont clash with cube outline
+
+    renderHi = function(){ //prints "HI"
+        if(!(y==0||y==width||z==width)){//avoid edges, so they dont clash with cube outline
             if(z>width*4/5){ //only render at the back of the cube
                 if (x<=width*3/20){ //H beam 1
                     if(y>=0){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
                 if (x>=width*6/20&&x<=width*9/20){ //H beam 2
                     if(y>=0){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
                 if (x>width*3/20&&x<width*6/20){ //H bar
                     if(y>=width*8/20&&y<=width*12/20){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
                 if (x>=width*13/20&&x<=width*16/20){ //I beam
                     if(y>=0){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
                 if (x>=width*11/20&&x<=width*18/20){ //I bar 1
                     if(y<=width*4/20){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
                 if (x>=width*11/20&&x<=width*18/20){ //I bar 2
                     if(y>width*16/20){
                         square.color=`rgba(0, 255, 0,${square.opacity})`
-                        square.render=1
+                        square.show=1
                     }
                 }
             }
@@ -78,9 +104,10 @@ function render3D(square){
         }
     }
     renderHi()
-    function firework(){
+
+    renderFirework = function(){
         //rendering firework code, to make the cube more opaque and smaller as it gets closer to the middle
-        square.render = 1
+        square.show = 1
 
         let newSize = square.size;
         let newOpacity = square.opacity; //as this goes up the firework gets brighter
@@ -103,15 +130,12 @@ function render3D(square){
             newOpacity=square.opacity
         }
         if (radius>halfwayPoint){ //this makes a sphere based on radius, which should max out at width/2 at the sides
-            newOpacity=0
+            square.show=0
         }
         
         /*
         To-Do:
-        Found r by using equation of a sphere to find the radius, but I'm not sure how to make it look good if half my code
-        relies on index and the other half relies on the actual pixel locations. Pushing this to main just to record my steps
-
-        also: equation for size could be -sqrt(r)+initialPixels, its a radical function but that sucks it's output would eventually
+        equation for size could be -sqrt(r)+initialPixels, its a radical function but that sucks it's output would eventually
         be negative. I need to find a rational function with a limit of y=0, where the y intercept is the initialPixels. 
         Maybe 1/(r+initialPixels) but idk. Update: took some time to test with a graphing calculator and this function looks good:
         f(r)=1/(.25*sqrt(r)+1/initialPixels) as the coefficient of sqrt(r) goes up, the size changes quicker
@@ -125,50 +149,25 @@ function render3D(square){
         square.size = newSize; //set new size per square
         square.color=`rgba(243, 146, 202,${square.opacity})` //save color again with new opacity
 
-        //change opacity and size of the squares
-        SQUARE.style.backgroundColor = `${square.color}`; //make the squares darker as they get further away
-        SQUARE.style.width = `${square.size}px`; 
-        SQUARE.style.height = `${square.size}px`; 
+        
     }
-    //firework()
-    
+    //renderFirework()
+    renderOutline()
     return
 }
 
 function printSquare(square) {//prints each square
-    //zIndex is a css property, Zindex is a property of custom Square class defined in outputCube() its confusing but zIndex ends up = to Zindex anyways
-    render3D(square)
+    render3D(square) //holds different example objects that can be rendered
+    //################
+    //####WARNING!#### if render3D(square) is removed, a default cube will appear where every dot is rendered, this eats up resources. 
+    //################ please make sure to lower the initial width value beforehand, 20 or less is recommended
 
-    //square.color = `rgba(243, 146, 202, 0)`//make all dots invisible by default
-    
-    
-    if (square.render<1){
+    if (square.show<1){ //if it is an unused square, it won't even have an element created. This saved so much time and space
         return 
     }
     
     const SQUARE = document.createElement("div");
-    SQUARE.classList.add("square");
-
-    SQUARE.style.marginLeft = `${square.marginLeft}px`;//distance between squares
-    SQUARE.style.marginTop = `${square.marginTop}px`; //distance between lines
-    SQUARE.style.zIndex = `${width-square.Zindex}px`; //move forwards in Z direction to avoid collision of elements
-
-    SQUARE.style.backgroundColor = `${square.color}`;
-
-    //firework()
-
-    //make a red outline around the cube:
-    let squareFirstOrLast = (square.Xindex == 0 || square.Xindex == width);
-    let lineFirstOrLast = (square.Yindex == 0 || square.Yindex == width);
-    let sliceFirstOrLast = (square.Zindex == 0 || square.Zindex == width);
-    if (lineFirstOrLast && squareFirstOrLast) { //make other edges red
-        SQUARE.style.backgroundColor = `rgba(255, 0, 0,${1 - ((square.Zindex - 3) / width)})`;
-    }
-    if (sliceFirstOrLast && (lineFirstOrLast || squareFirstOrLast)) { //make first and last face's edges red
-        SQUARE.style.backgroundColor = `rgba(255, 0, 0,${1 - ((square.Zindex - 3) / width)})`;
-    }
-
-    
+    square.render(SQUARE); //change the properties of each square element based on how I want it rendered
 
     WINDOW.appendChild(SQUARE);
 
@@ -181,15 +180,15 @@ function calculateSquareDistance(Zindex) { // x**0 == 1 so first case still work
 }//cube size will always remain the same by dynamically changing the factor by which squareDistance is shrunken
 //personal note: precalc function modelling actually helped me here lol ((xroot(.5))**z)/x would have a limit, and that limit is the end of the cube
 
-function outputCube() {
-    let squareDistance = 0; //starting square distance
+function outputCube() { //makes the cube's individual square objects, then calls printSquare() to display each one
+    let squareDistance = 0; //starting distance between squares, 0 so it lines up with the top left
     let sliceLocation = 0;  //keeps track of where current slice will be printed, first slice/line/square starts at the top left
     let squareYCoord = 0;   //keeps track of where current line will be printed in the slice, first line at the start of the slice
     let squareXCoord = 0;   //keeps track of where current square will be printed in the line, first square at the start of the line
 
     class Square { //each individual point has its own square object
         constructor(size, color, indices, squareXCoord, squareYCoord) {
-            this.render = 0; //if == 0, don't render square element, maybe add render method in square class
+            this.show = 1; //if == 0, don't render square element
             this.opacity = 5; //removed it from parameters because it was constant and cluttered the args in new Square(...)
             this.size = size;
             this.color = color;
@@ -201,30 +200,33 @@ function outputCube() {
             this.marginTop = squareYCoord;     //Y pixel position of square
         }
         
-        // render = function(){
-        //     const SQUARE = document.createElement("div");
-        //     SQUARE.classList.add("square");
+        render(element){ //element is the SQUARE element defined in printSquare()
+            element.classList.add("square");
 
-        //     SQUARE.style.marginLeft = `${square.marginLeft}px`;//distance between squares
-        //     SQUARE.style.marginTop = `${square.marginTop}px`; //distance between lines
-        //     SQUARE.style.zIndex = `${width-square.Zindex}px`; //move forwards in Z direction to avoid collision of elements
-        // }
+            element.style.marginLeft = `${this.marginLeft}px`;//distance between squares
+            element.style.marginTop = `${this.marginTop}px`; //distance between lines
+            element.style.zIndex = `${width-this.Zindex}px`; //move forwards in Z direction to avoid collision of elements
+            //change opacity and size of the squares
+            element.style.width = `${this.size}px`; 
+            element.style.height = `${this.size}px`; 
+            element.style.backgroundColor = `${this.color}`;
+        }
     }
 
     if (width < 2) {
         console.log("use more points to see the center");
     }
     // calculate and print the cube
-    for (let i = 0; i < width + 1; i++) { // turn this into recursion or width^3 somehow, current way is just too slow
-        squareDistance = calculateSquareDistance(i); //goes at start of each slice, each one has unique distance
+    for (let z = 0; z < width + 1; z++) { // turn this into recursion or width^3 somehow, current way is just too slow
+        squareDistance = calculateSquareDistance(z); //goes at start of each slice, each one has unique distance
         sliceLocation = (totalDistance - squareDistance * width) / 2; //this is how much to move each slice, changes per slice
         squareYCoord = sliceLocation; //squareYCoord is the line location, which is also each square's Y coordinate
 
-        for (let j = 0; j < width + 1; j++) {
+        for (let y = 0; y < width + 1; y++) {
             squareXCoord = sliceLocation; //position of first square of each line
 
-            for (let k = 0; k < width + 1; k++) {
-                const square = new Square(12, `rgba(243, 146, 202,1)`, [i,j,k], squareXCoord, squareYCoord); //make square object
+            for (let x = 0; x < width + 1; x++) {
+                const square = new Square(12, `rgba(243, 146, 202,1)`, [z,y,x], squareXCoord, squareYCoord); //make square object
                 printSquare(square); //print square
                 squareXCoord += squareDistance; //next square's position relative to the left side of the image, goes after each square is printed
             }
